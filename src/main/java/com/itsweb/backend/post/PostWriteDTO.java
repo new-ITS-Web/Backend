6d8c62ae7163553aa6650c37b1312762ddfe94a1
Jang Hyeon Seo <jang8195@naver.com>
2023-09-24 23:58:35 +0900

    CORSfeat/ 문ìconfiguration(origin=*)

--- src/main/java/com/itsweb/backend/post/PostWriteDTO.java
+++ src/main/java/com/itsweb/backend/post/PostWriteDTO.java
@@ -1,8 +1,6 @@
 package com.itsweb.backend.post;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,11 +10,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PostWriteDTO{
-    @NotEmpty(message = "제목은 비어있을 수 없습니다.")
+    @NotEmpty(message = "제목은 비어있으면 안됩니다.")
     @Size(max = 20, message = "제목은 20자 이하여야 합니다.")
     private String title;
 
-    @NotEmpty(message = "내용은 비어있을 수 없습니다.")
+    @NotEmpty(message = "내용은 비어있으면 안됩니다.")
     @Size(max = 1000, message = "내용은 1000자 이하여야 합니다.")
     private String content;
 }
