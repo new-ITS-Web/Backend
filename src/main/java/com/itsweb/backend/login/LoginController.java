fabbeb8bf851436d8c8c264c8bcbcde9fb2924a9
sanikani <sanikani@naver.com>
2023-10-03 21:10:36 +0900

    MethodArgumentNotValidException ExceptionHandler 처리

--- src/main/java/com/itsweb/backend/login/LoginController.java
+++ src/main/java/com/itsweb/backend/login/LoginController.java
@@ -1,6 +1,5 @@
 package com.itsweb.backend.login;
 
-import com.itsweb.backend.ValidationErrorHandler;
 import com.itsweb.backend.member.Member;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -9,27 +8,17 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 @Slf4j
 public class LoginController {
-
-    private final ValidationErrorHandler validationErrorHandler;
     private final LoginService loginService;
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@Valid @RequestBody LoginDTO loginDTO, HttpServletRequest request, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            Map<String, String> errorMap = validationErrorHandler.handleError(bindingResult);
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMap);
-        }
-
+    public ResponseEntity<?> login(@Valid @RequestBody LoginDTO loginDTO, HttpServletRequest request) {
         Member loginMember = loginService.loginCheck(loginDTO.getUserId(),loginDTO.getPassword());
         if (loginMember == null) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("아이디 또는 비밀번호 오류입니다.");
