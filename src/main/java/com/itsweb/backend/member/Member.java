6cffbc8dd76ed5b41e8bb7dc0374e37a6e89c3c4
sanikani <sanikani@naver.com>
2023-10-04 21:53:53 +0900

    feat: Spring Security 적용해서 password 암호화 Refactor: @Transactional service단에서 적용되게 변경 fix: loginCheck method NullPointerException 해결

--- src/main/java/com/itsweb/backend/member/Member.java
+++ src/main/java/com/itsweb/backend/member/Member.java
@@ -4,14 +4,15 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Getter
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
     private Long id;
 
     @Column(unique = true)
@@ -22,9 +23,10 @@ public class Member {
 
     private String password;
 
-    public void signUp(String userId,String username, String password) {
+    public void signUp(String userId, String username, String password, PasswordEncoder passwordEncoder) {
         this.userId = userId;
-        this.password = password;
+        this.password = passwordEncoder.encode(password);
         this.username = username;
     }
+
 }
--- src/main/java/com/itsweb/backend/member/MemberController.java
+++ src/main/java/com/itsweb/backend/member/MemberController.java
@@ -3,9 +3,8 @@ package com.itsweb.backend.member;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,17 +14,13 @@ import org.springframework.web.bind.annotation.*;
 public class MemberController {
 
     private final MemberService memberService;
+    private final PasswordEncoder bCryptPasswordEncoder;
 
     @PostMapping("/join")
-    @Transactional
     public ResponseEntity<?> join(@Valid @RequestBody MemberDTO memberDTO) {
         Member member = new Member();
-        member.signUp(memberDTO.getUserId(), memberDTO.getUsername(), memberDTO.getPassword());
-        if (memberService.isDuplicated(member)) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("아이디 또는 닉네임 중복입니다");
-        }
-        memberService.save(member);
-        return ResponseEntity.ok().body("회원가입 성공");
+        member.signUp(memberDTO.getUserId(), memberDTO.getUsername(), memberDTO.getPassword(),bCryptPasswordEncoder);
+        return memberService.join(member);
     }
 
 }
