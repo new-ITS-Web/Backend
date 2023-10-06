2c02129913093047f4b976b43b0ff093839abd36
sanikani <sanikani@naver.com>
2023-10-06 17:28:30 +0900

    feat: 데이터베이스로 세션 관리, 세션에 MemberVO 객체 전달하고 관리하도록 구현 chore: SecurityConfig deprecated 수정

--- src/main/java/com/itsweb/backend/configuration/SecurityConfig.java
+++ src/main/java/com/itsweb/backend/configuration/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.itsweb.backend.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,8 +18,8 @@ public class SecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf((csrf) -> csrf
-                .ignoringRequestMatchers(new AntPathRequestMatcher("/**")))
-                .formLogin().disable();
+                        .ignoringRequestMatchers(new AntPathRequestMatcher("/**")))
+                        .formLogin(Customizer.withDefaults());
         return http.build();
     }
     @Bean
