df1d50d83208dbcc56cb885e810129f59f066c94
sanikani <sanikani@naver.com>
2023-10-12 18:22:43 +0900

    feat: 로그인 유저 체크 인터셉터 구현 chore: 에러에 stacktrace 포함 안되게 변경

---
+++ src/main/java/com/itsweb/backend/configuration/WebConfig.java
@@ -0,0 +1,17 @@
+package com.itsweb.backend.configuration;
+
+import com.itsweb.backend.member.LoginCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/error", "/api/login", "/api/join", "/css/**", "/*.ico");
+    }
+}
